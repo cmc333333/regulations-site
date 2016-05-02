a3217534a07f6273cc1f874cf8836fcbe2b8d977
CM Lubinski <christopher.lubinski@gsa.gov>
2016-05-02 00:31:05 +0000

    -

--- regulations/uitests/definition_test.py
+++ regulations/uitests/definition_test.py
@@ -92,8 +92,8 @@ class DefinitionTest(BaseTest, unittest.TestCase):
         if len(headers_seen) == 3:
             self.assertIn(u'§1005.1(b)', headers_seen)
 
-        print(self.driver.find_element_by_xpath(
-            '//*[@id="1005-2-b-1"]/div[2]/div').get_attribute('innerHTML'))
+        print self.driver.find_element_by_xpath(
+            '//*[@id="1005-2-b-1"]/div[2]/div').get_attribute('innerHTML').decode('utf-8')
         definition_update_link = self.driver.find_element_by_xpath(
             '//*[@id="1005-2-b-1"]/div[2]/div/a')
         definition_text = self.driver.find_element_by_xpath(
